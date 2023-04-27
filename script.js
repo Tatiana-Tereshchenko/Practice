42ab27d93e1ceced80db963616ed60e13dd9e936
Tatiana-Tereshchenko <teresenkot33@gmail.com>
2023-04-27 17:32:58 +0300

    11

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function createProduct(product, callback) {
-    const id = Date.now();
-    const newProduct = { ...product, id };
-    callback(newProduct)
-}
-function logProduct(product) {
-    console.log(product);
-};
-function logTotalPrice({ price, quantity }) {
-    console.log( price * quantity);
-    return price * quantity
-}
-
-createProduct({ name: 'aple', price: 30, quantity: 3 }, logProduct);
-createProduct({ name: 'banana', price: 10, quantity: 2 }, logTotalPrice);
+const numbers = [5, 10, 15, 20];
+const newNubers = numbers.filter(number => number >10)
+console.log(newNubers);
